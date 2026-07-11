dc24a51d4110edeb4cd45a93e7ed3581c869fde6
Ragin <ragin666@pm.me>
2026-07-11 16:04:02 +0200

    Init

--- racingmanager-backend/src/main/kotlin/com/example/racingmanager/api/StatusPages.kt
+++ racingmanager-backend/src/main/kotlin/com/example/racingmanager/api/StatusPages.kt
@@ -5,15 +5,6 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respond
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ProblemDetail(
-    val type: String,
-    val title: String,
-    val status: Int,
-    val detail: String? = null,
-)
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
